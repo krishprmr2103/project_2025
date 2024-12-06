65c428e71e1b75bd0d067968e65fb80f8a0230a1
krishprmr2103 <krishprmr2004@gmail.com>
2024-12-06 14:44:59 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const Header =()=>{
+    
+    return(
+        <header>
+            <div class="logo">My Website</div>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </nav>
+        </header>
+    )
+}
+export default Header
