cd09eada0ad17cf827edfb6b5dd3af924cf7a1cc
krishprmr2103 <krishprmr2004@gmail.com>
2024-12-11 17:39:25 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,19 +1,34 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-const App =()=>{
-    
-    return(
-        <>
-        <Header />
-            <main>
-                <div class="content">
-                    <h1>Welcome to My Website</h1>
-                    <p>This is the main content area. Add your content here.</p>
-                </div>
-            </main>
-        <Footer />
-        </>
-    )
-}
-export default App
+// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Routes,Route } from "react-router-dom";
+
+import Header from "./components/Header.jsx";
+import About from "./components/About.jsx";
+import Contact from "./components/Contact.jsx"; // Corrected import
+import Books from "./components/Books.jsx";
+import Home from "./components/Home.jsx"; // New Home component;
+import Footer from "./components/Footer.jsx";
+import Content from "./components/Content.jsx";
+
+
+const App =  () => {
+  
+  return (
+    <>
+      <Header/>
+     
+      <div>
+        <Routes>
+        <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+      <Content/>
+
+      <Footer/> 
+    </>
+  );
+};
+export default App;
