84a58c0aef84c430da97c507c26b28081245b0af
krishprmr2103 <krishprmr2004@gmail.com>
2024-12-11 17:38:55 +0530

    Header.jsx

--- Header.jsx
+++ Header.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-const Header =()=>{
-    
-    return(
-        <header>
-            <div class="logo">My Website</div>
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+import { Routes,Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+const Header =  () => {
+  return (
+    <>
+      
+      <header>
+            <div class="logo">Marvels Books Store</div>
             <nav>
-                <a href="#">Home</a>
-                <a href="#">About</a>
-                <a href="#">Services</a>
-                <a href="#">Contact</a>
-            </nav>
+                {/* <a className="nav-link" href="/">Home</a>
+                <a className="nav-link" href="/books">Books</a>
+                <a className="nav-link" href="/about">About</a>
+                <a className="nav-link" href="contact">About</a> */}
+                <Link className='nav-link' to='/'>Home</Link>
+                <Link className='nav-link' to='/books'>Books Catagory</Link>
+                <Link className='nav-link' to='/about'>About</Link>
+                <Link className='nav-link' to='/contact'>About</Link>
+           
+           </nav>
         </header>
-    )
-}
-export default Header
+    </>
+  );
+};
+export default Header;
